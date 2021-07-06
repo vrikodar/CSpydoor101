2d0c0e7b12bc7d810245a267d2a0b8a9ca7613a7
z3r0day <73271976+SxNade@users.noreply.github.com>
2021-07-06 08:39:23 -0400

    Update v-engine.py

--- v-engine.py
+++ v-engine.py
@@ -1,4 +1,4 @@
-#By SxNade
+#Author: z3r0day
 #https://github.com/SxNade/Vaishnavastra
 #CONTRIBUTE
 
@@ -10,13 +10,17 @@ from termcolor import colored
 #importing the required Libraries
 
 golo = '''
- _    __      _      __                                 __            
-| |  / /___ _(_)____/ /_  ____  ____ __   ______ ______/ /__________ _
-| | / / __ `/ / ___/ __ \/ __ \/ __ `/ | / / __ `/ ___/ __/ ___/ __ `/
-| |/ / /_/ / (__  ) / / / / / / /_/ /| |/ / /_/ (__  ) /_/ /  / /_/ / 
-|___/\__,_/_/____/_/ /_/_/ /_/\__,_/ |___/\__,_/____/\__/_/   \__,_/  
+
+ / ____|/ ____|               | |               /_ |/ _ \/_ |
+| |    | (___  _ __  _   _  __| | ___   ___  _ __| | | | || |
+| |     \___ \| '_ \| | | |/ _` |/ _ \ / _ \| '__| | | | || |
+| |____ ____) | |_) | |_| | (_| | (_) | (_) | |  | | |_| || |
+ \_____|_____/| .__/ \__, |\__,_|\___/ \___/|_|  |_|\___/ |_|
+              | |     __/ |                                  
+              |_|    |___/                                   
+ 
                                 
-                                *By SxNade https://github.com/SxNade
+                                *z3r0day :: https://github.com/SxNade
  '''
 
 #defining a start Function that runs the relevant Function
